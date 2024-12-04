86b073536005d13fef5c7143c7e4f48aef56d278
RahulPrasad13400 <rahulprasad1399@gmail.com>
2024-12-04 10:58:49 +0530

    day 2 of building this project

--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -3,12 +3,16 @@ import Product from "./pages/Product";
 import HomePage from "./pages/HomePage";
 import Pricing from "./pages/Pricing";
 import PageNotFound from "./pages/PageNotFound";
+import AppLayout from './pages/AppLayout'
+import Login from "./pages/Login";
 export default function App() {
   return <BrowserRouter>
   <Routes>
     <Route path="product" element={<Product />} />
     <Route path="/" element={<HomePage />}/>
-    <Route path="price" element={<Pricing />} />
+    <Route path="pricing" element={<Pricing />} />
+    <Route path="/app" element={<AppLayout />} />
+    <Route path="/login" element={<Login />} />
     <Route path="*" element={<PageNotFound />} />
   </Routes>
   </BrowserRouter>
